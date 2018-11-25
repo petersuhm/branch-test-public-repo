cc2cd917a9235505d7064c9632647b1ace203f84
Peter Suhm <peter@suhm.dk>
2018-11-25 17:23:45 +0100

    initial commit

---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+function add_action($name, $callback)
+{
+    return null; // Dummy implementation
+}
+
+add_action(
+    'init', function () {
+        echo null; // Do nothing ...
+    }
+);
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class PluginTest extends TestCase
+{
+    public function testTrueIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
